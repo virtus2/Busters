ad477e3e08543ced7d1beb64de9ab71c2cbaaee2
virtus2 <khd1323@naver.com>
2022-09-15 23:42:54 +0900

    Initial commit

---
+++ Source/Busters/Busters.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Busters.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Busters, "Busters" );
+ 
---
+++ Source/Busters/BustersGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BustersGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ABustersGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ABustersGameMode();
+};
+
+
+
