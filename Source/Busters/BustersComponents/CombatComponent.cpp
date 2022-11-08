3fbd6f96d13693179129a7933a2cd64b1ab1592d
virtus2 <khd1323@naver.com>
2022-11-09 01:58:29 +0900

    feat: 무기 발사 기능 구현(WIP)

--- Source/Busters/BustersComponents/CombatComponent.cpp
+++ Source/Busters/BustersComponents/CombatComponent.cpp
@@ -1,12 +1,16 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+	// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "CombatComponent.h"
 
 #include "Busters/Character/BustersCharacter.h"
+#include "Busters/Weapon/BulletTypes.h"
 #include "Busters/Weapon/Weapon.h"
+#include "Busters/Weapon/WeaponTypes.h"
+#include "Kismet/GameplayStatics.h"
 #include "Kismet/KismetSystemLibrary.h"
 #include "Net/UnrealNetwork.h"
+#include "Sound/SoundCue.h"
 
 // Sets default values for this component's properties
 UCombatComponent::UCombatComponent()
@@ -26,7 +30,13 @@ void UCombatComponent::BeginPlay()
 	Super::BeginPlay();
 
 	// ...
-	
+	if(IsValid(Character))
+	{
+		if(Character->HasAuthority())
+		{
+			InitializeAmmo();
+		}
+	}
 }
 
 
@@ -35,7 +45,13 @@ void UCombatComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActo
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	if (IsValid(Character) && Character->IsLocallyControlled())
+	{
+		FHitResult HitResult;
+		TraceUnderCrosshairs(HitResult);
+		HitTarget = HitResult.ImpactPoint;
+		
+	}
 }
 
 void UCombatComponent::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
@@ -51,6 +67,128 @@ void UCombatComponent::OnRep_Weapon()
 	{
 		Character->UpdateWeaponMesh();
 	}
+}
+
+void UCombatComponent::TraceUnderCrosshairs(FHitResult& TraceHitResult)
+{
+	FVector2D ViewportSize;
+	if (IsValid(GEngine) && GEngine->GameViewport)
+	{
+		GEngine->GameViewport->GetViewportSize(ViewportSize);
+	}
+	FVector2D CrosshairLocation(ViewportSize.X / 2.f, ViewportSize.Y / 2.f);
+	FVector CrosshairWorldPosition;
+	FVector CrosshairWorldDirection;
+	bool bScreenToWorld = UGameplayStatics::DeprojectScreenToWorld(
+		UGameplayStatics::GetPlayerController(this, 0),
+		CrosshairLocation,
+		CrosshairWorldPosition,
+		CrosshairWorldDirection
+	);
+
+	if (bScreenToWorld)
+	{
+		FVector Start = CrosshairWorldPosition;
+
+		if (Character)
+		{
+			float DistanceToCharacter = (Character->GetActorLocation() - Start).Size();
+			Start += CrosshairWorldDirection * (DistanceToCharacter + 100.f);
+		}
+		FVector End = Start + CrosshairWorldDirection * TRACE_LENGTH;
+
+		TObjectPtr<UWorld> World = GetWorld();
+		if(IsValid(World))
+		{
+			World->LineTraceSingleByChannel(
+				TraceHitResult,
+				Start,
+				End,
+				ECollisionChannel::ECC_Visibility
+			);
+		}
+	}
+}
+
+void UCombatComponent::FireButtonPressed(bool bPressed)
+{
+	bFireButtonPressed = bPressed;
+	if(bFireButtonPressed)
+	{
+		FireWeapon();
+	}
+}
+
+void UCombatComponent::FireWeapon()
+{
+	if(CanFireWeapon())
+	{
+		// 총을 쏜 유저에게는 바로 발사 애니메이션을 재생한다.
+		if (IsValid(Character))
+		{
+			if (Character->GetADS())
+			{
+				Character->GetADSSkeletalMesh()->PlayAnimation(Weapon->FireAnimation, false);
+				Character->OnFireWeapon();
+			}
+			else
+			{
+				Character->GetOverlaySkeletalMesh()->PlayAnimation(Weapon->FireAnimation, false);
+			}
+			// LocalFireWeapon(HitTarget);
+		}
+
+		// 이후 Server, Multicast RPC를 호출해 모든 유저에게 발사 애니메이션을 재생한다.
+		ServerFireWeapon(HitTarget);
+	}
+}
+
+bool UCombatComponent::CanFireWeapon()
+{
+	if (!IsValid(Weapon)) return false;
+	return true;
+}
+
+void UCombatComponent::ServerFireWeapon_Implementation(const FVector_NetQuantize& TraceHitTarget)
+{
+	MulticastFireWeapon(TraceHitTarget);
+}
+
+void UCombatComponent::MulticastFireWeapon_Implementation(const FVector_NetQuantize& TraceHitTarget)
+{
+	// 소리 재생(클라, 서버)
+	if (IsValid(Character) && IsValid(Weapon) && IsValid(Weapon->FireSound))
+	{
+		UGameplayStatics::PlaySoundAtLocation(this, Weapon->FireSound, Character->GetActorLocation());
+		// 오버레이 메쉬 발사 애니메이션 재생(다른 유저)
+		if(!Character->IsLocallyControlled())
+		{
+			Character->GetOverlaySkeletalMesh()->PlayAnimation(Weapon->FireAnimation, false);
+		}
+	}
+	// 
+	if(IsValid(Character) && IsValid(Weapon) && Character->HasAuthority())
+	{
+		Weapon->Fire(TraceHitTarget, Character->GetADS());
+	}
+	
+	//LocalFireWeapon(TraceHitTarget);
+}
+
+void UCombatComponent::LocalFireWeapon(const FVector_NetQuantize& TraceHitTarget)
+{
+	if (IsValid(Character) && IsValid(Weapon))
+	{
+		UKismetSystemLibrary::PrintText(this, FText::FromString("Local Fire"));
+		Character->GetOverlaySkeletalMesh()->PlayAnimation(Weapon->FireAnimation, false);
+	}
+}
+
+void UCombatComponent::InitializeAmmo()
+{
+	AmmoMap.Emplace(EBulletType::EBT_5mm, Starting5mmAmmo);
+	AmmoMap.Emplace(EBulletType::EBT_7mm, Starting7mmAmmo);
+	AmmoMap.Emplace(EBulletType::EBT_9mm, Starting9mmAmmo);
 }
 
 FTransform UCombatComponent::GetWeaponADSTransform()
--- Source/Busters/BustersComponents/CombatComponent.h
+++ Source/Busters/BustersComponents/CombatComponent.h
@@ -6,8 +6,13 @@
 #include "Components/ActorComponent.h"
 #include "CombatComponent.generated.h"
 
+#define TRACE_LENGTH 80000.f
+
 class AWeapon;
 class ABustersCharacter;
+class USkeletalMeshComponent;
+enum class EWeaponType : uint8;
+enum class EBulletType : uint8;
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class BUSTERS_API UCombatComponent : public UActorComponent
@@ -17,12 +22,29 @@ class BUSTERS_API UCombatComponent : public UActorComponent
 public:
 	friend class ABustersCharacter;
 
+protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, ReplicatedUsing=OnRep_Weapon, Category = "Weapon")
 	TObjectPtr<AWeapon> Weapon;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Weapon")
+	int32 Starting5mmAmmo = 0;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Weapon")
+	int32 Starting7mmAmmo = 0;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Weapon")
+	int32 Starting9mmAmmo = 0;
 
 private:
 	UPROPERTY()
 	TObjectPtr<ABustersCharacter> Character;
+
+protected:
+	FVector HitTarget;
+
+	bool bFireButtonPressed;
+	bool bCanFire;
+
+	TMap<EBulletType, int32> AmmoMap;
+
 
 public:	
 	// Sets default values for this component's properties
@@ -39,6 +61,29 @@ protected:
 	UFUNCTION()
 	void OnRep_Weapon();
 
+	void TraceUnderCrosshairs(FHitResult& TraceHitResult);
+
+	//
+	// Fire
+	//
+	void FireButtonPressed(bool bPressed);
+
+	void FireWeapon();
+	bool CanFireWeapon();
+
+	UFUNCTION(Server, Reliable)
+	void ServerFireWeapon(const FVector_NetQuantize& TraceHitTarget);
+
+	UFUNCTION(NetMulticast, Reliable)
+	void MulticastFireWeapon(const FVector_NetQuantize& TraceHitTarget);
+
+	void LocalFireWeapon(const FVector_NetQuantize& TraceHitTarget);
+
+	//
+	// Ammo
+	// 
+	void InitializeAmmo();
+	
 public:
 	FTransform GetWeaponADSTransform();
 	inline TObjectPtr<AWeapon> GetWeapon() { return Weapon; };
